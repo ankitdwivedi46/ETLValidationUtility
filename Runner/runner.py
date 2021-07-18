fa8fe820a101305da36be4f5659a8506df270e83
Ankit <ankitdwivedi46@live.com>
2021-07-18 22:57:30 +0530

    Changes 18-07-2021

--- Runner/runner.py
+++ Runner/runner.py
@@ -1,4 +1,6 @@
 from Scripts.CountsCheck import CountsCheck
+from Scripts.DuplicatesCheck import DuplicatesCheck
+from Scripts.UniqueCheck import UniqueCheck
 
 
 print("---------------------------ETL VALIDATION UTILITY---------------------------")
@@ -14,12 +16,21 @@ while(1):
     check = input("Enter Type of Check(1 for Counts, 2 for Duplicates, 3 for Unique, 4 for Not Null, 5 for Data and E for Exit)")
     if(check=="E"):
         break
-    if(check=="1"):
+    elif(check=="1"):
         source_count_query = input("Enter Source Count Query")
         target_count_query = input("Enter Target Count Query")
         cc = CountsCheck(source_count_query, target_count_query)
         cc.count_validation()
-
+    elif(check == "2"):
+        source_dup_query = input("Enter Source Dup Query")
+        target_dup_query = input("Enter Target Dup Query")
+        dc = DuplicatesCheck(source_dup_query, target_dup_query)
+        dc.duplicate_validation()
+    elif(check == "3"):
+        source_uniq_query = input("Enter Source Unique Query")
+        target_uniq_query = input("Enter Target Unique3 Query")
+        uc = UniqueCheck(source_uniq_query, target_uniq_query)
+        uc.unique_validation()
 
 
 
